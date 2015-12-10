d931be2cc7aab123a4b7cca6aa91ec9c304abd67
nguyen-trung-quan <nguyen-trung-quan@ap0646.local>
2015-12-10 10:44:47 +0900

    Initial Commit

---
+++ libfdr/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  libfdr
+//
+//  Created by nguyen-trung-quan on 2015/12/10.
+//  Copyright © 2015年 nguyen-trung-quan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
